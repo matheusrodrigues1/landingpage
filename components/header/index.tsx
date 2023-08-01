0a288e5dc10a5f2e31de8730176367a31c61bc9d
matheusrodrigues1 <matech00@outlook.com>
2023-08-01 20:51:14 -0300

    first commit

---
+++ components/header/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Logo from "../../public/images/agencia.svg";
+import Link from "next/link";
+import Styles from './header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.logotipo}>
+        <Image src={Logo} />
+      </div>
+      <div className={Styles.menu}>
+            <Link href="/">Home</Link>
+            <Link href="/">O que fazemos</Link>
+            <Link href="/">Cases</Link>
+      </div>
+      <div className={Styles.action}>
+        <button className={Styles.button}>Fale conosco</button>
+      </div>
+    </div>
+  )
+
+}
+
+export default Header;
